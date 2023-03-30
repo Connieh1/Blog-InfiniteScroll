cdd603fc54acdf6d208aa3b3c0fe3463a80f49f7
Connie Hyman <connie2630a@gmail.com>
2023-03-29 22:43:13 -0700

    Add scroll

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const postsContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page =1;
 
 // Fetch posts from API
 async function getPosts() {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
+  const result = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
 
-  const data = await res.json();
+  const data = await result.json();
 
   return data;
 }
@@ -29,9 +29,32 @@ async function showPosts() {
     </div>
     `;
 
-    postsContainer.appendChild(postEl)
+    postsContainer.appendChild(postEl);
   })
+}
+
+// Show loader & fetch more posts
+function showLoading(){
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(()=> {
+      page++;
+      showPosts();
+    }, 300)
+  }, 1000);
 }
 
 // Show initial posts
 showPosts()
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } =
+  document.documentElement;
+
+  if(scrollTop + clientHeight + scrollHeight - 5) {
+    showLoading();
+  }
+})
